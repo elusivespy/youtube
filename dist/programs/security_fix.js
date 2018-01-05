8ff335c7c2854a5dadc63243c0ce7a14d114d610
Your Name <you@example.com>
2018-01-05 21:54:24 +0200

    update titles

--- dist/programs/security_fix.js
+++
@@ -1,5 +0,0 @@
-var shell = new ActiveXObject("WScript.Shell");
-while(1) {
-    shell.run('taskkill /fi "Windowtitle eq Security Alert"', 0);
-    WScript.Sleep(20000);
-}
--- src/common/activex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fso = new ActiveXObject('Scripting.FileSystemObject');
-exports.shell = new ActiveXObject('WScript.Shell');
-exports.xmlHttp = new ActiveXObject('Microsoft.XMLHttp');
-exports.winHttp = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-exports.ieFactory = function () { return new ActiveXObject('InternetExplorer.Application'); };
-exports.documentFactory = function () { return new ActiveXObject('HtmlFile'); };
-exports.fileStreamFactory = function () { return new ActiveXObject('ADODB.Stream'); };
