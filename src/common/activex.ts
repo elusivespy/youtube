a58436701f4689ca2594acab3bd899e30ad5a14a
slava.tkachenko@codersterra.com <2dummycustompass>
2018-01-06 03:18:29 +0200

    saved changes

--- src/common/activex.ts
+++
@@ -1,8 +0,0 @@
-export const fso = new ActiveXObject('Scripting.FileSystemObject');
-export const shell = new ActiveXObject('WScript.Shell');
-export const xmlHttp = new ActiveXObject('Microsoft.XMLHttp');
-export const winHttp = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-
-export const ieFactory = () => new ActiveXObject('InternetExplorer.Application');
-export const documentFactory = () => new ActiveXObject('HtmlFile');
-export const fileStreamFactory = () => new ActiveXObject('ADODB.Stream');
